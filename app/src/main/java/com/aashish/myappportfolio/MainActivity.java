fd2889ab3db449fb9e050be757340caf08f5a5eb
aashishvanand <aashishvanand@gmail.com>
2016-01-26 17:55:16 +0530

    final

--- app/src/main/java/com/aashish/myappportfolio/MainActivity.java
+++ app/src/main/java/com/aashish/myappportfolio/MainActivity.java
@@ -19,41 +19,39 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button button = (Button) findViewById(R.id.button);
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4 = (Button) findViewById(R.id.button4);
-        Button button5 = (Button) findViewById(R.id.button5);
-        Button button6 = (Button) findViewById(R.id.button6);
+        Button SpotifyBtn = (Button) findViewById(R.id.SpotifyBtn);
+        Button ScoresBtn = (Button) findViewById(R.id.ScoresBtn);
+        Button LibraryBtn = (Button) findViewById(R.id.LibraryBtn);
+        Button BuilditBtn = (Button) findViewById(R.id.BuilditBtn);
+        Button ReaderBtn = (Button) findViewById(R.id.ReaderBtn);
+        Button CAPSTONEBtn = (Button) findViewById(R.id.CAPSTONEBtn);
 
-
-
-        button.setOnClickListener(new View.OnClickListener(){
+        SpotifyBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 DisplayText(getResources().getString(R.string.SpotifyText));
             }
         });
-        button2.setOnClickListener(new View.OnClickListener(){
+        ScoresBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 DisplayText(getResources().getString(R.string.ScoresText));
             }
         });
-        button3.setOnClickListener(new View.OnClickListener(){
+        LibraryBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 DisplayText(getResources().getString(R.string.LibraryText));
             }
         });
-        button4.setOnClickListener(new View.OnClickListener(){
+        BuilditBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 DisplayText(getResources().getString(R.string.BuilditText));
             }
         });
-        button5.setOnClickListener(new View.OnClickListener(){
+        ReaderBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 DisplayText(getResources().getString(R.string.ReaderText));
             }
         });
-        button6.setOnClickListener(new View.OnClickListener(){
+        CAPSTONEBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 DisplayText(getResources().getString(R.string.CapstoneText));
             }
@@ -87,4 +85,5 @@ public class MainActivity extends AppCompatActivity {
     void DisplayText(String s){
         Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
     }
+
 }
